38463cc602c716b61de97968c4513d7ad1ae9dce
Himanshu Sharma <142489823+Himanshusharma47@users.noreply.github.com>
2023-10-18 10:26:23 +0530

    update

--- resources/views/layouts/leftlist.blade.php
+++ resources/views/layouts/leftlist.blade.php
@@ -10,24 +10,8 @@
             @endforeach
         </ul>
     </div>
-    {{-- @endif
+    
 
-    <div class="items">
-        <ul>
-            <li>TVs</li>
-            <li>Dishwasher</li>
-            <li>Ranges</li>
-            <li>Computer</li>
-            <li>Blu-ray & DVD Player</li>
-            <li>Projectors</li>
-            <li>Hometheater System</li>
-            <li>Cameras</li>
-            <li>Camcorders</li>
-            <li>Washer & Dryers</li>
-            <li>Refrigerators</li>
-            <li>Microwaves</li>
-        </ul>
-    </div>--}}
 
 
 </div>
